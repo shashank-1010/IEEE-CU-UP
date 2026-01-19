032ab135f1244a59c217d96a91d78179a6a523b9
mrxbyte0110 <shashank.work247@gmail.com>
2026-01-19 13:26:22 +0530

    Update Home.tsx

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -241,17 +241,7 @@ const DrawCircleText = () => {
             fill="none"
             className="absolute -left-2 -right-2 -top-2 bottom-0 translate-y-1 w-[120%] h-auto"
           >
-            <motion.path
-              initial={{ pathLength: 0 }}
-              whileInView={{ pathLength: 1 }}
-              transition={{
-                duration: 1.25,
-                ease: "easeInOut",
-              }}
-              d="M142.293 1C106.854 16.8908 6.08202 7.17705 1.23654 43.3756C-2.10604 68.3466 29.5633 73.2652 122.688 71.7518C215.814 70.2384 316.298 70.689 275.761 38.0785C230.14 1.37835 97.0503 24.4575 52.9384 1"
-              stroke="#00629B"
-              strokeWidth="3"
-            />
+            
           </svg>
         </span>{" "}<br></br>
         for Humanity
