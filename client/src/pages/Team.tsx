3c23f7de8e16951575dd9bf7a336b789b8da4ee9
Sampoorn Tripathi <tripathisampoorn1@gmail.com>
2026-01-21 20:59:55 +0530

    Update Team.tsx

--- client/src/pages/Team.tsx
+++ client/src/pages/Team.tsx
@@ -40,7 +40,7 @@ export default function Team() {
       role: "Branch Counsellor, IEEE Student Branch ",
       department: "School of Computer Science and Engineering",
       email: "priya.sharma@ieee-cu.edu",
-      imageUrl: "/images/team/sir2.png",
+      imageUrl: "/images/team/sir3.png",
       category: "faculty",
     },
     {
@@ -49,7 +49,7 @@ export default function Team() {
       role: "Faculty Advisor",
       department: "School of Computer Science and Engineering",
       email: "priya.sharma@ieee-cu.edu",
-      imageUrl: "/images/team/sir3.png",
+      imageUrl: "/images/team/sir2.png",
       category: "faculty",
     },
     
